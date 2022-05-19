2367712bcbf97e9117573f7c0904dbfd4bd5cb6e
Güvenç Usanmaz <gusanmaz@gmail.com>
2022-05-19 12:57:37 +0300

    Create HTMLTree.java

---
+++ HTMLTree.java
@@ -0,0 +1,25 @@
+public class HTMLTree {
+    private HTMLNode root;
+
+    public HTMLTree(HTMLNode root){
+        this.root = root;
+    }
+
+    public HTMLTree(){
+        this(null);
+    }
+
+    
+    // Bu metod bu sinifta saklanan HTML agacinin icerigini String olarak dondurmektedir.
+    // Bu metodu yazmak icin bu sinif icinde baska yardimci baska metod/metodlar tanimlayabilirsiniz.
+    // Bu metodu recursive olarak yazmaniz beklenmektedir.
+    // Bu metodu daha kolay kodlayabilmeniz icin string birlestirme operatoru olan `+` operatorunun
+    // nasil kullanildigina ve String sinifi metodlarindan biri olan repeat metodunu arastirmaniz
+    // tavsiye edilmektedir.
+    // Bu metodun dogru kodlanmasi durumunda uretmesi beklenen cikti Main.java sinifi icindeki kodlar 
+    // ve bu kodlarin uretecegi program ciktisi incelenerek gorulebilir. 
+    public String GetHTML(){
+        // Metodun icini uygun sekilde doldurunuz.
+    }
+
+}
